51ef1d3ca74aba7fba18b6ebe7c9023eb50ebb2b
Sanya <alxsk-b@yandex.ru>
2024-02-25 19:03:26 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Репозиторий на GitHub называется 'Hello world!'");
+        System.out.println("Новый commit");
     }
 }
